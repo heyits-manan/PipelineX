2ac57aad018b07d53d61e07a398b5d5fb2b5459a
Manan Chataut <mananchataut@gmail.com>
2026-04-05 16:16:10 +0530

    remove todo comments from errors.go

--- internal/video/errors.go
+++ internal/video/errors.go
@@ -2,9 +2,6 @@ package video
 
 import "errors"
 
-// TODO: Keep shared domain errors here.
-// TODO: Add more errors only when they support real control flow.
-
 var (
 	ErrNotFound     = errors.New("not found")
 	ErrConflict     = errors.New("conflict")
